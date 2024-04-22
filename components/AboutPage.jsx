cb636c0989b283c7277f41f53bb056f6d9520be3
Emmacodec <danielemmanuel7553@gmail.com>
2024-04-22 17:22:01 +0100

    working on about component

--- components/AboutPage.jsx
+++ components/AboutPage.jsx
@@ -2,8 +2,66 @@ import React from 'react'
 
 const AboutPage = () => {
   return (
-    <div>
-      
+    <div id='about' className='w-full md:h-screen p-2 flex items-center py-16'>
+      <div className=''>
+        <h2 className='uppercase tracking-widest text-xl text-[#5651e5] underline'>About R.a.g.e</h2>
+        <p className='py-4'>The Universal Declaration of Human Rights affirms that education is a fundamental human right for everyone and this right was further detailed in the Convention against Discrimination in Education. What exactly does that mean?</p>
+        <p className='py-2 text-gray-600 font-bold'>Why is education a fundamental human right?</p>
+        <p className='py-2 text-gray-600'>
+          * Quality education aims to ensure the development of a fully-rounded human being.
+        </p>
+        <p className='py-2 text-gray-600'>
+          * It is one of the most powerful tools in lifting socially excluded children and adults out of poverty and into society. UNESCO data shows that if all adults completed secondary education, globally the number of poor people could be reduced by more than half.
+          </p>
+        <p className='py-2 text-gray-600'>
+          * It narrows the gender gap for girls and women. A UN study showed that each year of schooling reduces the probability of infant mortality by 5 to 10 per cent.
+          </p>
+        <p className='py-2 text-gray-600'>
+          * For this human right to work there must be equality of opportunity, universal access, and enforceable and monitored quality standards.
+          </p>
+
+          <p className='py-4 font-bold'>What does the right to education entail?</p>
+          <p className='py-2 text-gray-600'>
+            * Primary education that is free, compulsory and universal
+            </p>
+          <p className='py-2 text-gray-600'>
+            * Secondary education, including technical and vocational, that is generally available, accessible to all and progressively free
+            </p>
+          <p className='py-2 text-gray-600'>
+            * Higher education, accessible to all on the basis of individual capacity and progressively free
+            </p>
+          <p className='py-2 text-gray-600'>
+            * Fundamental education for individuals who have not completed education
+            </p>
+          <p className='py-2 text-gray-600'>
+            * Professional training opportunities
+            </p>
+          <p className='py-2 text-gray-600'>
+            * Equal quality of education through minimum standards
+            </p>
+          <p className='py-2 text-gray-600'>
+            * Quality teaching and supplies for teachers
+            </p>
+          <p className='py-2 text-gray-600'>
+            * Adequate fellowship system and material condition for teaching staff
+            </p>
+          <p className='py-2 text-gray-600'>
+            * Freedom of choice
+            </p>
+            <p className='py-4 font-bold'>What is the current situation?</p>
+            <p className='py-2 text-gray-600'>
+              * About 258 million children and youth are out of school, according to UIS data for the school year ending in 2018. The total includes 59 million children of primary school age, 62 million of lower secondary school age and 138 million of upper secondary age.
+              </p>
+            <p className='py-2 text-gray-600'>
+              * 155 countries legally guarantee 9 years or more of compulsory education
+              </p>
+            <p className='py-2 text-gray-600'>
+              * Only 99 countries legally guarantee at least 12 years of free education
+              </p>
+            <p className='py-2 text-gray-600'>
+              * 8.2% of primary school age children does not go to primary school  Only six in ten young people will be finishing secondary school in 2030 The youth literacy rate (15-24) is of 91.73%, meaning 102 million youth lack basic literacy skills.
+              </p>
+      </div>
     </div>
   )
 }
